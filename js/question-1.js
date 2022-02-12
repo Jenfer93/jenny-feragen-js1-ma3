304f21726a48560f435ebe2f55490ae90655f823
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-02-12 14:17:16 +0100

    question 1 done

---
+++ js/question-1.js
@@ -0,0 +1,7 @@
+const getReminder = (a, b) => {
+  return a % b;
+}
+
+const result = getReminder(3, 5);
+
+console.log(result);
---
+++ js/question-2.js
@@ -0,0 +1,4 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=3cf889ba48b84fc3ac8316e0b65c63f4";
+
+const 
+
