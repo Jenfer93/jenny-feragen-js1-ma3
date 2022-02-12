91ace948c6e96516e87baf626590da3f51bbe23a
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-02-12 15:40:59 +0100

    done

--- js/question-2.js
+++ js/question-2.js
@@ -1,4 +1,27 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=3cf889ba48b84fc3ac8316e0b65c63f4";
 
-const 
+const resultContainer = document.querySelector(".container");
+
+async function getGames() {
+
+  const response = await fetch(url);
+
+  const allGames = await response.json();
+
+  const games = allGames.results; 
+
+  resultContainer.innerHTML = "";
+
+  for (let i = 0; i < 8; i++) {
+    //console.log(games[i].tags.length);
+
+    resultContainer.innerHTML += `<div class="games"> 
+                                  <h2>${games[i].name}<h2>
+                                  <p> Rating: ${games[i].rating}</p>
+                                  <p> Number of tags: ${games[i].tags.length}</p>
+                                  </div>`
+  }
+}
+
+getGames();
 
